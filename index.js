abb94ac0dadb5dc1c5a9f788a08bf6b1befcb43c
ihposey12 <ihposey@crimson.ua.edu>
2021-06-23 14:58:23 -0700

    merge sort done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const mergeSort = (array) => {
+    let midpoint = array.length / 2
+    let first = array.slice(0, midpoint)
+    let second = array.slice(midpoint, array.length)
 
+    if(array.length < 2) {
+        console.log(array)
+    } else {
+        console.log(merge(mergeSort(first), mergeSort(second)))
+    }
+}
